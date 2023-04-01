5145278bc6ff00cee650324ab6ab1d43b71f8eed
DESKTOP-3PQGQUK\Home <gerli_jyrioo@hotmail.com>
2023-04-01 10:18:34 +0300

    some changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+ // templateUrl: './header.component.html',
+  template: `<p>header works! {{ someText }} </p>`,
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  someText = "My app is really nice!";
 }
