b5275fdca9693561ab5f2f2b04d10c13895098bf
DESKTOP-3PQGQUK\Home <gerli_jyrioo@hotmail.com>
2023-04-01 11:59:56 +0300

    style

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,7 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
  // templateUrl: './header.component.html',
-  template: `<p>header works! {{ someText }} </p>`,
+  //template: `<p>header works! {{ someText }} </p>`,
+  template: `
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="logo"> <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" style="width:30px; height:25px;"/></a>
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>`,
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
